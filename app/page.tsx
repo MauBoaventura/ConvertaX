92563102f6c6ee05e31576f32cd525f910826a62
Mauboa <mba097@gmail.com>
2024-07-10 18:09:12 -0300

    feat: add dashboard to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import CardInvestiment from "@/components/custom/cards/cardInvestiment";
+
 export default function Home() {
+
   return (
-    <div className="flex justify-center">
-      <div className="flex flex-col justify-center w-[60vw] p-8">
-        {/* <CustomBarChart /> */}
-        {/* <CustomLineChart /> */}
-        {/* <InvestmentForm /> */}
-      </div>
+    <div className="flex justify-center items-center">
+      <CardInvestiment />
     </div>
-  )
+  );
 }
